07fea3c550ca0100736589c3d6bac13af1c49046
ThozamileMad <118363197+ThozamileMad@users.noreply.github.com>
2024-03-06 19:18:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
-num = int(input("Enter number: "))
-solution = 2 
-index_num = 0 
-run = True
-lst = [num + 1 for num in range(num)][2:] 
-while run: 
-    lst_value = lst[index_num] 
-    solution *= lst_value 
-    index_num += 1
-    
-    if len(lst) == index_num:
-        print(solution)
-        run = False
+
+
